3f216912695a8fb432e3ebb74a4c1e51e0a22fd3
Hannah Noble <hannah.noble1@engineering.digital.dwp.gov.uk>
2025-11-18 12:31:12 +0000

    Self appoint process for ACS team iteration 1
    GitOrigin-RevId: 221fa264608c05a114aa97c89b0a7c8dd9b392fc

--- app/routes.js
+++ app/routes.js
@@ -1,2 +1,52 @@
 
+//
+// For guidance on how to create routes see:
+// https://prototype-kit.service.gov.uk/docs/create-routes
+//
 
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+// Add your routes here
+
+router.post('/self-service/iteration-1/poa-or-lpa', function(request, response) {
+
+    var poaEpa = request.session.data['poa-epa']
+    if (poaEpa == "yes-epa"){
+        response.redirect("/self-service/iteration-1/kickout/epa-process")
+    } else if (poaEpa == "yes-poa"){
+        response.redirect("/self-service/iteration-1/kickout/poa-process")
+    } else if (poaEpa == "no"){
+        response.redirect("/self-service/iteration-1/guardian-deputyship")
+    }
+})
+
+router.post('/self-service/iteration-1/guardian-deputyship', function(request, response) {
+
+    var guardianDeputy = request.session.data['guardian-deputy']
+    if (guardianDeputy == "yes"){
+        response.redirect("/self-service/iteration-1/kickout/scottish-deputy-process")
+    } else if (guardianDeputy == "no"){
+        response.redirect("/self-service/iteration-1/assessed-hcp")
+    }
+})
+
+router.post('/self-service/iteration-1/assessed-hcp', function(request, response) {
+
+    var assessedHcp = request.session.data['assessed-hcp']
+    if (assessedHcp == "yes"){
+        response.redirect("/self-service/iteration-1/appointee-application")
+    } else if (assessedHcp == "no") {
+        response.redirect("/self-service/iteration-1/managing-affairs")
+    }
+})
+
+router.post('/self-service/iteration-1/managing-affairs', function(request, response) {
+
+    var helpManaging = request.session.data['help-managing-affairs']
+    if (helpManaging == "yes"){
+        response.redirect("/self-service/iteration-1/appointee-application")
+    } else if (helpManaging == "no") {
+        response.redirect("/self-service/iteration-1/kickout/contact-DWP")
+    }
+})

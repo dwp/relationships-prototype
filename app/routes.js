b7b4435b112b6dc33c3c09573d650b1b61b8082b
Hannah Noble <hannah.noble1@engineering.digital.dwp.gov.uk>
2025-12-22 15:21:09 +0000

    updates pre worm
    GitOrigin-RevId: 16c1a0bb5183d9e0243cb98e6a0e4e13762a70ca

--- app/routes.js
+++ app/routes.js
@@ -12,7 +12,7 @@ const router = govukPrototypeKit.requests.setupRouter()
 router.post('/self-service/iteration-1/individual-org', function(request, response) {
 
     var role = request.session.data['individual-org']
-    if (role == "individual"){
+    if (role == "personal-rel"){
         response.redirect("/self-service/iteration-1/poa-or-lpa")
     } else if (role == "corp-org"){
         response.redirect("/self-service/iteration-1/kickout/cab")
@@ -36,7 +36,7 @@ router.post('/self-service/iteration-1/guardian-deputyship', function(request, r
     var guardianDeputy = request.session.data['guardian-deputyship']
     if (guardianDeputy == "deputyship"){
         response.redirect("/self-service/iteration-1/kickout/deputyship-process")
-    } else if (guardianDeputy == "scottish-guardianship"){
+    } else if (guardianDeputy == "guardianship"){
         response.redirect("/self-service/iteration-1/kickout/scottish-process")
     } else if (guardianDeputy == "no"){
         response.redirect("/self-service/iteration-1/assessed-hcp")
@@ -57,8 +57,28 @@ router.post('/self-service/iteration-1/managing-affairs', function(request, resp
 
     var helpManaging = request.session.data['help-managing-affairs']
     if (helpManaging == "yes"){
-        response.redirect("/self-service/iteration-1/appointee-application")
+        response.redirect("/self-service/iteration-1/need-fulltime-help")
     } else if (helpManaging == "no") {
-        response.redirect("/self-service/iteration-1/kickout/contact-DWP")
+        response.redirect("/self-service/iteration-1/help-communicating")
+    }
+})
+
+router.post('/self-service/iteration-1/need-fulltime-help', function(request, response) {
+
+    var helpCommunicating = request.session.data['fulltime-help']
+    if (helpCommunicating == "all-time"){
+        response.redirect("/self-service/iteration-1/appointee-application")
+    } else if (helpCommunicating == "some-time") {
+        response.redirect("/self-service/iteration-1/kickout/implicit-alternate-enq")
+    }
+})
+
+router.post('/self-service/iteration-1/help-communicating', function(request, response) {
+
+    var helpCommunicating = request.session.data['help-communicating']
+    if (helpCommunicating == "adjustments-needed"){
+        response.redirect("/self-service/iteration-1/kickout/reasonable-adjustment")
+    } else if (helpCommunicating == "none-needed") {
+        response.redirect("/self-service/iteration-1/kickout/dwp-enquiry")
     }
 })
